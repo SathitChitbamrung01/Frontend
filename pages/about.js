35f70ce135184c48aac4ede6e0d9a2929fc6b8f4
SathitChitbamrung01 <matsumotoryuuji2@gmail.com>
2023-06-07 10:04:16 +0700

    about_Contact

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image';
+import Script from 'next/script';
+import Link from 'next/link';
+export default function about() {
+  return (
+    <>
+    </>
+  )
+}
